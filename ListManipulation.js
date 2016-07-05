dcaaa97002664ce8cbab66e9d5d200546f8ef889
Kristi Marks <kristithecoder@gmail.com>
2016-07-05 15:06:59 -0700

    Add files via upload

---
+++ ListManipulation.js
@@ -0,0 +1,260 @@
+"use strict";
+var List = function(list) {
+   
+    var _list = [];
+    this.setList = function(list) {
+        if (this.isListValid(list)) {
+            _list = list;
+        } else
+            throw "Invalid list: call getValidationErrors(list) for details";
+    }
+    this.getList = function() {
+        return _list;
+    }
+    if (list != undefined)
+        this.setList(list);
+
+
+}; //end constructor
+
+List.prototype.createFromHtml = function(tableSelected) {
+    //pull off properties into array
+    if (tableSelected && tableSelected.length > 0) {
+        var properties = this.getHeadersFromHtml(tableSelected[0].children);
+
+        //for each row in collection
+        for (var i = 1; i < tableSelected.length; i++) {
+            var rowparent = tableSelected[i];
+            var newObject = this.emptyObject(properties);
+            newObject.html = rowparent.outerHTML;
+            var row = rowparent.children;
+
+            for (var cell = 0; cell < row.length; cell++) {
+                if (properties[cell] != undefined) {
+                    if (row[cell].children[0] !== undefined) {
+                        //get child property value
+                        var chlidNodeName = row[cell].children[0].nodeName;
+                        var childClassName = row[cell].children[0].className;
+
+                        switch (chlidNodeName) {
+                        case "SELECT":
+                            newObject[properties[cell]] = row[cell].children[0].selectedOptions[0].value;
+                            break;
+                        case "INPUT":
+                            if (childClassName === "check-box")
+                                newObject[properties[cell]] = row[cell].children[0].checked;
+                            break;
+                        default:
+                            newObject[properties[cell]] = row[cell].children[0].innerText.trim();
+                            break;
+                        }
+                    } else {
+                        var string = row[cell].outerText.replace("\\n", "").trim();
+                        if (row[cell].hasAttribute("dataType"))
+                            string = this.convertToType(string, row[cell].getAttribute("dataType"));
+                        newObject[properties[cell]] = string;
+                    }
+                }
+            }
+
+            this.getList().push(newObject);
+
+        }
+    }
+
+};
+
+List.prototype.sort = function (sublist, attrName, asc) {
+    attrName = attrName.trim();
+    attrName = attrName.replace(/\s+/g, '');
+    if (!sublist) {
+        sublist = this.getList();
+    }
+    if (asc === undefined || asc === null)
+        asc = true;
+    if (sublist.length < 1)
+        return sublist;
+    if (attrName == undefined || sublist[0][attrName] == undefined) {
+        throw 'Attribute to sort on does not exist, or is not defined by the object';
+
+    }
+
+    sublist.sort(this.compare(attrName));
+    if (!asc)
+        sublist.reverse();
+    return sublist;
+};
+
+List.prototype.search = function(attributes, target) {
+   
+    var filteredList = [];
+
+    if (attributes === undefined||attributes==null) {
+        attributes = Object.getOwnPropertyNames(this.getList()[0]);
+    } else {
+        var listAttributes = Object.getOwnPropertyNames(this.getList()[0]);
+        attributes.forEach(function(attr) {
+            var res = 0;
+            if (listAttributes.indexOf(attr) <= -1)
+                throw "Attribute [" + attr + "] is not valid to search on";
+
+        });
+    }
+    if (target !== undefined && target !== null) {
+        target = target.toLowerCase();
+        filteredList =this.getList().filter(function(object) {
+            //check for attributes which contain target
+            for (var attr in attributes) {
+                attributes[attr]=attributes[attr].trim();
+                var searchString = object[attributes[attr]].toString();
+                if (typeof searchString === 'string') {
+                    searchString = searchString.toLowerCase();
+                    if (searchString.indexOf(target) > -1)
+                        return true;
+                }
+            }
+            return false;
+        });
+
+    }
+    return filteredList;
+
+};
+
+List.prototype.toPages = function(sublist, entriesPerPage) {
+    if (sublist === undefined || sublist === null)
+        sublist = this.getList();
+    var pages = [];
+// ReSharper disable once QualifiedExpressionMaybeNull
+    for (var i = 0; i < sublist.length; i += entriesPerPage) {
+        pages.push(sublist.slice(i, i + entriesPerPage));
+    }
+    //ensure empty list has atleast 1 page
+    if (sublist.length <= 0)
+        pages.push([]);
+    return pages;
+};
+
+List.prototype.isListValid = function(list) {
+
+    if (list === undefined || list === null)
+        return false;
+    if (!Array.isArray(list))
+        return false;
+    var valid;
+    
+    list.forEach(function(entry) {
+        if (typeof entry !== 'object') {
+            valid = false;
+        }
+    });
+    if (valid != undefined)
+        return false;
+    if (list.length>0) {
+        var propNames = JSON.stringify(Object.getOwnPropertyNames(list[0]));
+        for (var i = 1; i < list.length; i++) {
+            var objectPropNames = JSON.stringify(Object.getOwnPropertyNames(list[i]));
+            if (objectPropNames !== propNames) {
+                return false;
+            }
+
+        }
+    }
+    return true;
+};
+
+
+List.prototype.getValidationErrors = function(list) {
+
+    var errors = [];
+    if (!Array.isArray(list)) {
+        errors.push("data passed in is not an array");
+    } else {
+        list.forEach(function(entry) {
+            if (typeof entry !== 'object') {
+                if (errors.indexOf("Array is not consistantly of type object") <= 0)
+                    errors.push("Array is not consistantly of type object");
+            }
+
+        });
+        //verify list objects all have the same properties
+        var propNames = JSON.stringify(Object.getOwnPropertyNames(list[0]));
+
+        for (var i = 1; i < list.length; i++) {
+            var objectPropNames = JSON.stringify(Object.getOwnPropertyNames(list[i]));
+            if (objectPropNames !== propNames) {
+                errors.push("Member at index " + i + " does not have consistant properties " +
+                    "with member at index 0");
+            }
+        }
+
+    }
+    return errors;
+};
+
+List.prototype.emptyObject = function(headers) {
+    var object = {};
+    if (!headers)
+        headers = Object.getOwnPropertyNames(this.getList()[0]);
+    for (var entry in headers) {
+        object[headers[entry]] = "";
+    }
+    return object;
+};
+
+List.prototype.getHeadersFromHtml = function(headerRow) {
+
+    // var headerRow = htmlCollection[0].children;
+
+    var headers = [];
+    for (var entry in headerRow) {
+
+        if (headerRow[entry].outerText != undefined) {
+            var string = headerRow[entry].outerText.trim();
+            string = string.replace(/\s+/g, '');
+            headers.push(string);
+
+        }
+    }
+    return headers;
+};
+
+List.prototype.compare = function(attrName) {
+    return function(a, b) {
+
+        var typea = typeof a[attrName];
+        var typeb = typeof b[attrName];
+        if (typea === typeb) {
+            switch (typea) {
+                case "string":
+                   
+                        return a[attrName].localeCompare(b[attrName]);
+                 
+            case "boolean":
+                if ((a[attrName] && b[attrName]) || (!a[attrName] && !b[attrName]))
+                    return 0;
+                if (a[attrName] && !b[attrName])
+                    return 1;
+                else {
+                    return -1;
+                }
+            default:
+                return a[attrName] - b[attrName];
+            }
+
+        }
+        throw "cannot compare " + typea + " and " + typeb;
+
+    }
+};
+List.prototype.convertToType= function(toConvert, type) {
+    switch (type) {
+    case "date":
+        var date = Date.parse(toConvert);
+        if (date !== NaN)
+            return date;
+        break;
+        default:
+            return toConvert.toString();
+    }
+}
---
+++ tableSorterSetup.js
@@ -0,0 +1,61 @@
+﻿$(document).ready(
+   function() {
+        /*** Building view object***/
+        var tableView = new TableView();
+        tableView.tableSelector = " .sortable";
+        tableView.rowSelector = " .sortable tr";
+        tableView.tableBodySelector = ".sortable tbody";
+        tableView.pagerUISelector = ".pagination";
+        tableView.numRecordsSelector = "#recordsPerPage";
+        tableView.headersSelector = ".header";
+        tableView.searchSelector = "#search";
+        tableView.sortIconAscending = "icon-sort_down gray lg";
+        tableView.sortIconDescending = "icon-sort_up gray lg";
+        tableView.submitFormSelector = ".submitRows";
+        tableView.submitButtonSelector = ".submitButton";
+        tableView.clearSelectionSelector = ".clearSelected";
+        tableView.highlightCssClass = "outline";
+        tableView.numSelectedSelector = "numSelected";
+        tableView.setUpHighlightRow();
+        tableView.createList();
+        tableView.buildPager();
+        tableView.setUpPager();
+        tableView.setUpSortSearchEvents();
+        tableView.setUpSubmitSelected();
+       
+        /**End Building view Object**/
+        /**Tab functionality**/
+        if ($("#tabUI").get(0)) {
+            if (tableView.list.getList()[0]["Active?"]!==undefined) {
+                var inactiveList = tableView.list.search(["Active?"], "false");
+                var activeList = tableView.list.search(["Active?"], "true");
+                $("#activeTab").addClass('active');
+                tableView.list.setList(activeList);
+                tableView.displayed = activeList;
+                tableView.buildTable();
+                tableView.buildPager();
+                tableView.setUpPager();
+
+                $("#tabUI li").click(function() {
+                    $(this).siblings().removeClass('active');
+                    $(this).addClass('active');
+                    if (this.id === "inactiveTab") {
+                        tableView.list.setList(inactiveList);
+                        tableView.displayed = inactiveList;
+                    } else {
+                        tableView.list.setList(activeList);
+                        tableView.displayed = activeList;
+                    }
+
+                    tableView.buildTable();
+                    tableView.buildPager();
+                    tableView.setUpPager();
+                    tableView.removeSortIcons();
+                    tableView.resetSearch();
+
+                });
+            }
+        }
+       
+   });
+ 
